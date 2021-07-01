1edfbfd7191eabe3fc0b7418d2ce088fc2072b64
go-max07 <johnmetry17@gmail.com>
2021-07-01 12:02:27 +0300

    added navigation bar to checklist.html

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+    const next = document.querySelector('.next');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-links li')
+
+    next.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+
+
+    navLinks.forEach(link, index) => {
+
+    }
+
+
+
+
+
+}
---
+++ js/index.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("picImg");
+  var dots = document.getElementsByClassName("cool");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" s1-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " s1-white";
+}
